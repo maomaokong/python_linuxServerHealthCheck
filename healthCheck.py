4430db25c947fc8b658686b2ce4b6a88451c7942
Desmond Teoh <maomaokong@gmail.com>
2018-10-08 15:52:37 +0800

    Minor adjustment

--- healthCheck.py
+++ healthCheck.py
@@ -12,65 +12,67 @@
 #########################################################################################
 
 import os
-import datetime
+import datetime as dt
 import smtplib
-from email.MIMEMultipart import MIMEMultipart
-from email.MIMEText import MIMEText
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
 
-lsFromAddr = 'abc@def.com'
-lsToAddr = 'mno@pqr.com'
-msg = MIMEMultipart()
-msg['From'] = lsFromAddr
-msg['To'] = lsToAddr
-msg['Subject'] = 'Server Health Check Report'
 
-lsServerName = os.uname()[1].upper()
-llCPU_Uti = round(float(os.popen('''
-                                grep 'cpu ' /proc/stat 
-                                | awk '{usage = ($2 + $4) * 100 / ($2 + $4 + $5)} END {print usage}' 
-                                ''').readline()), 2)
-llMem_Uti = round(float(os.popen('''
-                                free -m | grep Mem: 
-                                | awk '{mem} = ($3 / $2) * 100 END {print mem}'
-                                ''').readline()), 2)
+def main():
+    from_addr = 'abc@def.com'
+    to_addr = 'mno@pqr.com'
+    msg = MIMEMultipart()
+    msg['From'] = from_addr
+    msg['To'] = to_addr
+    msg['Subject'] = 'Server Health Check Report'
 
-if (llCPU_Uti >= 80 or llMem_Uti >= 80):
-    lbSendEmail = 1
-    lsBgColor = 'red'
-else:
-    lbSendEmail = 0
-    lsBgColor = 'blue'
+    server_name = os.uname()[1].upper()
+    cpu_uti = round(float(os.popen('''
+                                    grep 'cpu ' /proc/stat | awk '{usage = ($2 + $4) * 100 / ($2 + $4 + $5)} END {print usage}' 
+                                    ''').readline()), 2)
+    mem_uti = round(float(os.popen('''
+                                    free -m | grep Mem: | awk '{mem = ($3 / $2) * 100} END {print mem}'
+                                    ''').readline()), 2)
 
-lsOutputReport = """
-<HTML>
-<HEAD>
-    <TITLE>Server Health Check Report</TITLE>
-</HEAD>
-<BODY background-color:peachbuff>
-    <H2><font color='#99000' face='Microsoft Tai le'>Server Health Check Report</font></H2>
-    <table border=1 cellpadding=0 cellspacing=0>
-        <tr bgcolor=gray align=center>
-            <td><b>Server Name</b></td>
-            <td><b>Avg.CPU Utilisation</b></td>
-            <td><b>Memory Utilisation</b></td>
-        </tr>
-        <tr bgcolor={0}>
-            <td>{1}</td>
-            <td>{2}</td>
-            <td>{3}</td>            
-        </tr>
-    </table>
-</BODY>
-</HTML>
-""".format(lsBgColor, lsServerName, llCPU_Uti, llMem_Uti)
-msg.attach(MIMEText(lsOutputReport, 'html'))
+    if cpu_uti >= 80 or mem_uti >= 80:
+        send_email = True
+        bg_color = 'red'
+    else:
+        send_email = False
+        bg_color = 'blue'
 
-# print HTML Report
+    report_output = """
+    <HTML>
+    <HEAD>
+        <TITLE>Server Health Check Report</TITLE>
+    </HEAD>
+    <BODY background-color:peachbuff>
+        <H2><font color='#99000' face='Microsoft Tai le'>Server Health Check Report</font></H2>
+        <table border=1 cellpadding=0 cellspacing=0>
+            <tr bgcolor=gray align=center>
+                <td><b>Server Name</b></td>
+                <td><b>Avg.CPU Utilisation</b></td>
+                <td><b>Memory Utilisation</b></td>
+            </tr>
+            <tr bgcolor={0}>
+                <td>{1}</td>
+                <td>{2}</td>
+                <td>{3}</td>            
+            </tr>
+        </table>
+    </BODY>
+    </HTML>
+    """.format(bg_color, server_name, cpu_uti, mem_uti)
+    msg.attach(MIMEText(report_output, 'html'))
 
-if (lbSendEmail == 1):
-    server = smtplib.SMTP('{SMTP Server Name}')
-    server.sendmail(lsFromAddr, lsToAddr, msg.as_string())
-    server.quit()
-    print("{0} - The Health Check Report sent!!".format(datetime.datetime.now()))
-else:
-    print("{0} - The Health Check Report failed to send out!!".format(datetime.datetime.now()))
+    if send_email:
+        server = smtplib.SMTP('{SMTP Server Name}')
+        server.sendmail(from_addr, to_addr, msg.as_string())
+        server.quit()
+        print("{0} - The Health Check Report sent!!".format(dt.datetime.now()))
+    else:
+        print("{0} - The Health Check Report not required!!".format(dt.datetime.now()))
+
+
+if __name__ == '__main__':
+    main()
